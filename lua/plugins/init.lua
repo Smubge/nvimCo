a943ff96bd68af68ca7392f4a8a536ba32cfb8f3
Smubge <jonathanblh938@gmail.com>
2023-02-25 14:29:01 -0500

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -59,6 +59,7 @@ return
   {'navarasu/onedark.nvim'},
   {'Mofiqul/dracula.nvim'},
   --CMP
+
  { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" }, -- The completion plugin
    { "hrsh7th/cmp-buffer", commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" }, -- buffer completions
    { "hrsh7th/cmp-path", commit = "447c87cdd6e6d6a1d2488b1d43108bfa217f56e1" }, -- path completions
