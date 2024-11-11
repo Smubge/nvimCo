2fb0cf4f0db00391a5cd6b66295e5ab869e5d14c
Smubge <87445504+Smubge@users.noreply.github.com>
2024-11-11 09:07:17 -0500

    Unity works
    (Heres how) (install dotnet sdk, dotnet runtime, mono, paste the code, and do export PATH=$PATH:/usr/share/dotnet and export DOTNET_ROOT=/usr/share/dotnet

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -138,34 +138,32 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local servers = {"rust_analyzer", "clangd", "jsonls","svelte", "jdtls", "pyright", "kotlin_language_server", "tsserver", "html", "astro", "ltex", "emmet_language_server"}
 
-
 for _, server in pairs(servers) do
   require("lspconfig")[server].setup {capabilities = capabilities}
 end
-
-
 
 local root_files = { "*.project.json", "sourcemap.json"}
 if not table.unpack then
   table.unpack = unpack
 end
 
-
-
 local isLuauUsed = false;
 local luau_def_location = "/home/smubge/luau-lsp/globalTypes.d.lua"
 local luau_docs_location = "/home/smubge/luau-lsp/api-docs.json"
---[[ local pid = vim.fn.getpid() ]]
---[[]]
+local pid = vim.fn.getpid() 
+
 local omnisharp_location = "/home/smubge/.local/share/nvim/mason/bin/omnisharp"
 local omnisharp_mono_location = '/home/smubge/.local/share/nvim/mason/bin/omnisharp-mono'
 
 require("lspconfig")["omnisharp"].setup {
   cmd = {
-    omnisharp_location
+    omnisharp_mono_location
   },
-  use_modern_net = false,
+  use_modern_net = true,
+  -- enable_ms_build_load_projects_on_demand = true,
+
 }
+
 
 require("lspconfig")["luau_lsp"].setup {
   root_dir = nvim_lsp.util.root_pattern(table.unpack(root_files)),
@@ -223,87 +221,83 @@ require("lspconfig")["lua_ls"].setup {
 local pid = vim.fn.getpid()
 
 -- First Example unity 
-local myCapabilities = vim.tbl_deep_extend(
-    "force",
-    require('cmp_nvim_lsp').default_capabilities(),
-    {
-        workspace = {
-            didChangeWatchedFiles = {
-                dynamicRegistration = true,
-            },
-        },
-    }
-)
+-- local myCapabilities = vim.tbl_deep_extend(
+--     "force",
+--     require('cmp_nvim_lsp').default_capabilities(),
+--     {
+--         workspace = {
+--             didChangeWatchedFiles = {
+--                 dynamicRegistration = true,
+--             },
+--         },
+--     }
+-- )
 -- omnisharp ryan.issac.g https://ryanisaacg.com/code/nvim-and-unity/
- --[[ require("lspconfig")["omnisharp"].setup { ]]
- --[[     cmd = { ]]
- --[[         "mono", ]]
- --[[         omnisharp_location, ]]
- --[[     capabilities = myCapabilities, ]]
- --[[         "--languageserver", ]]
- --[[         "--hostPID", ]]
- --[[         tostring(pid), ]]
- --[[     }, ]]
- --[[]]
- --[[     -- Enables support for reading code style, naming convention and analyzer ]]
- --[[     -- settings from .editorconfig. ]]
- --[[     enable_editorconfig_support = true, ]]
- --[[]]
- --[[     -- If true, MSBuild project system will only load projects for files that ]]
- --[[     -- were opened in the editor. This setting is useful for big C# codebases ]]
- --[[     -- and allows for faster initialization of code navigation features only ]]
- --[[     -- for projects that are relevant to code that is being edited. With this ]]
- --[[     -- setting enabled OmniSharp may load fewer projects and may thus display ]]
- --[[     -- incomplete reference lists for symbols. ]]
- --[[     enable_ms_build_load_projects_on_demand = false, ]]
- --[[]]
- --[[     -- Enables support for roslyn analyzers, code fixes and rulesets. ]]
- --[[     enable_roslyn_analyzers = false, ]]
- --[[]]
- --[[     -- Specifies whether 'using' directives should be grouped and sorted during ]]
- --[[     -- document formatting. ]]
- --[[     organize_imports_on_format = false, ]]
- --[[]]
- --[[     -- Enables support for showing unimported types and unimported extension ]]
- --[[     -- methods in completion lists. When committed, the appropriate using ]]
- --[[     -- directive will be added at the top of the current file. This option can ]]
- --[[     -- have a negative impact on initial completion responsiveness, ]]
- --[[     -- particularly for the first few completion sessions after opening a ]]
- --[[     -- solution. ]]
- --[[     enable_import_completion = false, ]]
- --[[]]
- --[[     -- Specifies whether to include preview versions of the .NET SDK when ]]
- --[[     -- determining which version to use for project loading. ]]
- --[[     sdk_include_prereleases = true, ]]
- --[[]]
- --[[     -- Only run analyzers against open files when 'enableRoslynAnalyzers' is ]]
- --[[     -- true ]]
- --[[     analyze_open_documents_only = false, ]]
- --[[ } ]]
+ -- require("lspconfig")["omnisharp"].setup { 
+ --     cmd = { 
+ --         "mono", 
+ --         omnisharp_location, 
+ --     capabilities = myCapabilities, 
+ --         "--languageserver", 
+ --         "--hostPID", 
+ --         tostring(pid), 
+ --     }, 
+ --     -- Enables support for reading code style, naming convention and analyzer 
+ --     -- settings from .editorconfig.
+ --     use_modern_net = false,
+ --     enable_editorconfig_support = true, 
+ --     -- If true, MSBuild project system will only load projects for files that 
+ --     -- were opened in the editor. This setting is useful for big C# codebases 
+ --     -- and allows for faster initialization of code navigation features only 
+ --     -- for projects that are relevant to code that is being edited. With this 
+ --     -- setting enabled OmniSharp may load fewer projects and may thus display 
+ --     -- incomplete reference lists for symbols. 
+ --     enable_ms_build_load_projects_on_demand = false, 
+ --     -- Enables support for roslyn analyzers, code fixes and rulesets.
+ --     enable_roslyn_analyzers = false,
+ --     -- Specifies whether 'using' directives should be grouped and sorted during
+ --     -- document formatting. 
+ --     organize_imports_on_format = false, 
+ --     -- Enables support for showing unimported types and unimported extension 
+ --     -- methods in completion lists. When committed, the appropriate using 
+ --     -- directive will be added at the top of the current file. This option can
+ --     -- have a negative impact on initial completion responsiveness,
+ --     -- particularly for the first few completion sessions after opening a
+ --     -- solution.
+ --     enable_import_completion = false,
+ --     -- Specifies whether to include preview versions of the .NET SDK when
+ --     -- determining which version to use for project loading.
+ --     sdk_include_prereleases = true,
+ --
+ --     -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+ --     -- true
+ --     analyze_open_documents_only = false,
+ -- } 
 
  -- Second example dzfrias https://dzfrias.dev/blog/neovim-unity-setup
 
---[[ require('lspconfig').omnisharp.setup { ]]
---[[   cmd = { ]]
---[[     'mono', ]]
---[[     '--assembly-loader=strict', ]]
---[[     omnisharp_location, ]]
---[[   }, ]]
---[[   -- Assuming you have an on_attach function. Delete this line if you don't. ]]
---[[   use_mono = true, ]]
---[[ } ]]
+-- require('lspconfig').omnisharp.setup { 
+--   cmd = { 
+--     'mono', 
+--     '--assembly-loader=strict', 
+--     omnisharp_location, 
+--   }, 
+--   -- Assuming you have an on_attach function. Delete this line if you don't. 
+--   use_mono = true, 
+-- } 
 
 
---Third example for real https://spaceandtim.es/code/nvim_unity_setup/
+-- Third example for real https://spaceandtim.es/code/nvim_unity_setup/
 -- Omnisharp/C#/Unity
---[[ local omnisharp_bin = "/opt/omnisharp-roslyn/run" ]]
---[[ require'lspconfig'.omnisharp.setup{ ]]
---[[     on_attach = on_attach, ]]
---[[     flags = { ]]
---[[       debounce_text_changes = 150, ]]
---[[     }, ]]
---[[     cmd = { omnisharp_location, "--languageserver" , "--hostPID", tostring(pid) }; ]]
---[[ } ]]
+-- local omnisharp_bin = "/opt/omnisharp-roslyn/run" 
+-- require'lspconfig'.omnisharp.setup{ 
+--     on_attach = on_attach, 
+--     flags = { 
+--       debounce_text_changes = 150, 
+--     }, 
+--     cmd = { omnisharp_location, "--languageserver" , "--hostPID", tostring(pid) };
+--     use_modern_net = false
+--  } 
 -- Fourht  example 
 
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -311,9 +305,19 @@ vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("OmnisharpHook", {}),
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client.name == "omnisharp" then
+    if client.name == "omnisharp" or client.name == "omnisharp_mono" or client.name == "omnisharp-mono" then
       client.server_capabilities.semanticTokensProvider = nil
     end
   end,
 })
 
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   group = vim.api.nvim_create_augroup("IncreaseTimeout", {}),
+--   callback = function(args)
+--     local client = vim.lsp.get_client_by_id(args.data.client_id)
+--     if client and client.name == "omnisharp" then
+--       client.config.flags = { allow_incremental_sync = true }
+--       client.config.init_options = { timeout = 300000 } -- Increase to 5 minutes if needed
+--     end
+--   end,
+-- })
